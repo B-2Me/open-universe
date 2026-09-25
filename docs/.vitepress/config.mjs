8097cdba84c5bc8bdf4d5242e344f9661c380ab9
Nathan <123835325+nater0000@users.noreply.github.com>
2026-09-25 11:09:07 -0700

    Add new link with favicon to configuration

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -64,6 +64,11 @@ export default defineConfig({
         icon: { svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/></svg>' },
         link: '/interference#contact',
         ariaLabel: 'Contact Me'
+      },
+      {
+        icon: { svg: '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><image href="/images/btwo-favicon.png" width="24" height="24" /></svg>' },
+        link: 'https://btwo.me',
+        ariaLabel: 'Published by btwo.me'
       }
     ]
   },
