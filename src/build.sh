34a980d47c0edd56771b4d56dfc7506a3b8b9f9b
Nathan <123835325+nater0000@users.noreply.github.com>
2026-09-26 11:05:02 -0700

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -7,7 +7,21 @@ mkdir -p ../docs/public/wasm/wake/
 emcc planck.c \
   -O3 \
   -s WASM=1 \
-  -s EXPORTED_FUNCTIONS="['_init_grid', '_tick', '_get_pixel_buffer_pointer', '_set_dissipation', '_set_thermal_limit', '_set_render_layer']" \
+  -s EXPORTED_FUNCTIONS="[
+    '_init_grid', 
+    '_tick', 
+    '_get_pixel_buffer_pointer', 
+    '_set_dissipation', 
+    '_set_thermal_limit', 
+    '_set_render_layer', 
+    '_clear_grid', 
+    '_randomize_grid', 
+    '_set_node', 
+    '_get_node', 
+    '_get_engine_version', 
+    '_get_grid_width', 
+    '_get_grid_height'
+  ]" \
   -s EXPORTED_RUNTIME_METHODS="['ccall', 'cwrap']" \
   -o ../docs/public/wasm/wake/planck.js
 
